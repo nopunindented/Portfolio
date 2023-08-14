e36967e19eec835d2e6e5c6394f280c2bdfcff4d
umerindented <umerfiaz251@gmail.com>
2023-08-13 19:15:25 -0600

    "animation in lines"

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -2,6 +2,14 @@ import React from "react";
 
 export default function HomePage() {
     return(
-        <div className="homebody"/>
+        <div className="homebody">
+            <div className="umerportfolio">Umer's Portfolio</div>
+            <div className="projectsheader">Projects</div>
+            <div className="technologiesheader">Technologies</div>
+            <div className="workexpheader">Work Experience</div>
+            <div class="growinglineone"/>
+            <div class="growinglinetwo"/>
+            <div class="growinglinethree"/>
+        </div>
     )
 }
