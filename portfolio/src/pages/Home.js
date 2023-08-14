dee81224610ad6e6fac9502294f1585ea852cb82
umerindented <umerfiaz251@gmail.com>
2023-08-14 15:32:27 -0600

    "text set up"

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Slide from '@mui/material';
 
 export default function HomePage() {
     return(
@@ -12,6 +13,12 @@ export default function HomePage() {
             <div class="growinglinethree"/>
             <div class="growinglinefour"/>
             <div class="growinglinefive"/>
+            <div className= 'hi'>Hi,</div>
+            <div className= 'iamumer'>I'm Umer!</div>
+            <p className= 'mydescription'>
+                I'm a 3rd year software engineering student
+                at the University of Alberta.
+                I enjoy creating applications that tackle social problems, help university students & clubs, or are just cool in general :)</p>
         </div>
     )
 }
