8a6ad84f98900f1400f1dc3a3bc0559fb53d03af
umerindented <umerfiaz251@gmail.com>
2023-08-16 20:14:02 -0600

    "pulsing around icon and zoom too"

--- portfolio/src/pages/themesong.js
+++ portfolio/src/pages/themesong.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from 'react';
 import mytheme from './homeimages/mytheme.mp3';
 import music from './homeimages/music.svg';
+import Zoom from '@mui/material/Zoom'
 
 function ClickIcon() {
   return <div key={Date.now()} className="circle pulse"></div>;
@@ -28,7 +29,11 @@ export default function ThemeSong() {
     <div>
       <div onClick={handlePlayButtonClick}>
         {clicked && <ClickIcon />}
-        <img src={music} className='musicicon' alt="hello" />
+        <Zoom 
+          in={true}
+          timeout={1000}>
+          <img src={music} className='musicicon' alt="hello" />
+        </Zoom>
       </div>
     </div>
   );
