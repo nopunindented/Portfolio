939b2df45f43d893fb4b1d8812e5c1bb054022be
umerindented <umerfiaz251@gmail.com>
2023-08-21 21:45:50 -0600

    "switch and animation set"

--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,24 +1,30 @@
 import React from "react";
+import { useState } from "react";
 import ThemeSong from "./themesong";
 
 
 export default function ProjectPage() {
+    const [isHovered, setIsHovered] = useState(false);
+
     return(
         <div>
 
-        <div className="thriftaidprojecy">
-            <div className="thriftaidbox" />
+            <div
+            className="thriftaidproject"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            >
+            <div className={`thriftaidbox ${isHovered ? 'hovered' : ''}`} />
             <div className="thriftaidboxsecondary" />
-            <div className="projectnamethrift">ThriftAid</div>
-            <div className="reactskillthrift">React</div>
-            <div className="nodeskillthrift">Node.js</div>
-            <div className="typescriptskillthrift">TypeScript</div>
-            <div className="mongodbskillthrift">MongoDB</div>
-            <div className="jwtauthskillthrift">JWT Authentication</div>
-            <div className="reduxskillthrift">Redux</div>
-            <div className="expressskillthrift">Express</div>
-            <div className="awsskillthrift">AWS</div>
-
+            <div className={`projectnamethrift ${isHovered ? 'hovered' : ''}`}>ThriftAid</div>
+            <div className={`reactskillthrift ${isHovered ? 'hovered' : ''}`}>React</div>
+            <div className={`nodeskillthrift ${isHovered ? 'hovered' : ''}`}>Node.js</div>
+            <div className={`typescriptskillthrift ${isHovered ? 'hovered' : ''}`}>TypeScript</div>
+            <div className={`mongodbskillthrift ${isHovered ? 'hovered' : ''}`}>MongoDB</div>
+            <div className={`jwtauthskillthrift ${isHovered ? 'hovered' : ''}`}>JWT Authentication</div>
+            <div className={`reduxskillthrift ${isHovered ? 'hovered' : ''}`}>Redux</div>
+            <div className={`expressskillthrift ${isHovered ? 'hovered' : ''}`}>Express</div>
+            <div className={`awsskillthrift ${isHovered ? 'hovered' : ''}`}>AWS</div>
         </div>
 
         </div>
